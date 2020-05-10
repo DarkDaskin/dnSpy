c24507e279c00ac4f70d29652e3169c7c59fcc62
Dark Daskin <darkdaskin@gmail.com>
2020-05-10 19:41:41 +0300

    Hex inspector settings cosmetic fix.

--- Extensions/dnSpy.HexInspector/Settings/HexInspectorSettingsView.xaml.cs
+++ Extensions/dnSpy.HexInspector/Settings/HexInspectorSettingsView.xaml.cs
@@ -1,7 +1,16 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace dnSpy.HexInspector.Settings {
 	public partial class HexInspectorSettingsView : UserControl {
 		public HexInspectorSettingsView() => InitializeComponent();
+		
+		void Grid_Loaded(object sender, RoutedEventArgs e) {
+			// Work-around for broken RelativeSource.
+			var target = (DependencyObject)sender;
+			var binding = BindingOperations.GetBinding(target, FrameworkElement.HeightProperty);
+			BindingOperations.SetBinding(target, FrameworkElement.HeightProperty, binding);
+		}
 	}
 }
